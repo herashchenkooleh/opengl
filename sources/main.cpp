e4b8689f7806f747cf579e5d2cbd0e5a6799c602
Oleg Gerashchenko <o.gerashchenko@dragonlk.com>
2020-01-30 17:08:36 +0200

    Try imgui

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,6 +2,9 @@
 #include "glfw/glfw3.h"
 #include "glm/glm.hpp"
 #include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <iostream>
 
 int main()
 {
@@ -12,7 +15,7 @@ int main()
 	}
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, gl::TRUE_);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -27,13 +30,47 @@ int main()
 		return -1;
 	}
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
 		glfwPollEvents();
+		gl::ClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+		gl::Clear(gl::COLOR_BUFFER_BIT);
+
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// render your GUI
+		ImGui::Begin("Demo window");
+		ImGui::Button("Hello!");
+		ImGui::End();
+
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		gl::Viewport(0, 0, display_w, display_h);
+		glfwSwapBuffers(window);
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return EXIT_SUCCESS;
 }
